a6cf290fdc863e864d70bc5e45c677c6c77e0be6
Mateusz Pietkiewicz <mtp@hicron.com>
2024-06-06 10:39:26 +0200

    env variables renamed

--- config/config.py
+++ config/config.py
@@ -6,7 +6,6 @@ load_dotenv()
 class Config: # We need to create a Config class for our config to become modular (.config.from_object())
     def __str2bool(value):
         return value.lower() in ("true", "1")
-
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{os.getenv("LOGIN")}:{os.getenv("PASSWORD")}@{os.getenv("HOST")}/{os.getenv("DB_NAME")}'
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    IS_DEBUG = __str2bool(os.getenv("IS_DEBUG"))
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{os.getenv("REST_API_DB_LOGIN")}:{os.getenv("REST_API_DB_PASSWORD")}@{os.getenv("REST_API_DB_HOST")}:{os.getenv("REST_API_DB_PORT")}/{os.getenv("REST_API_DB_NAME")}'
+    SECRET_KEY = os.getenv("REST_API_SECRET_KEY")
+    IS_DEBUG = __str2bool(os.getenv("REST_API_IS_DEBUG"))
